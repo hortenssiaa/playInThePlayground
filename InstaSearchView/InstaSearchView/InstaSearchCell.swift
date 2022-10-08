3b963bce6c486d4ac1dcbe34ddb4dc47d147ff83
Hakyung Sohn <hortenssiaa@gmail.com>
2022-10-08 23:54:17 +0900

    Added Insta App

---
+++ InstaSearchView/InstaSearchView/InstaSearchCell.swift
@@ -0,0 +1,12 @@
+//
+//  InstaSearchCell.swift
+//  InstaSearchView
+//
+//  Created by Hakyung Sohn on 2022/10/07.
+//
+
+import UIKit
+
+class InstaSearchCell: UICollectionViewCell {
+    
+}
---
+++ InstaSearchView/InstaSearchView/ResultCell.swift
@@ -0,0 +1,12 @@
+//
+//  ResultCell.swift
+//  InstaSearchView
+//
+//  Created by Hakyung Sohn on 2022/10/07.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+    
+}
