c682f8bb4ec30dd406fa3da6a4f4212f74e51782
Hakyung Sohn <hortenssiaa@gmail.com>
2022-10-14 16:13:04 +0900

    Commit Modal version

---
+++ AppleFramework_Modal/AppleFramework/FrameworkDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FrameworkDetailViewController.swift
+//  AppleFramework
+//
+//  Created by Hakyung Sohn on 2022/10/14.
+//
+
+import UIKit
+import SafariServices
+
+class FrameworkDetailViewController: UIViewController {
+
+    @IBOutlet weak var frameworkImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var learnmoreButton: UIButton!
+    
+    var framework: AppleFramework = AppleFramework(name: "Unknown", imageName: "", urlString: "", description: "Unknown")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        frameworkImageView.image = UIImage(named: framework.imageName)
+        titleLabel.text = framework.name
+        descriptionLabel.text = framework.description
+    }
+
+    @IBAction func learnMoreTapped(_ sender: Any) {
+        guard let url = URL(string: framework.urlString) else {
+            return
+        }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+    }
+}
