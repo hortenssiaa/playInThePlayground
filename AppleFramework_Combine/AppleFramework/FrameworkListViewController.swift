8f2331402984f415160dd73d622a66e547952921
Hakyung Sohn <hortenssiaa@gmail.com>
2022-10-24 20:51:56 +0900

    Changed with combine

--- AppleFramework_Combine/AppleFramework/FrameworkListViewController.swift
+++ AppleFramework_Combine/AppleFramework/FrameworkListViewController.swift
@@ -19,11 +19,14 @@ class FrameworkListViewController: UIViewController {
     
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
-    let list: [AppleFramework] = AppleFramework.list
-    
     // Combine
+    // 비동기처리에 유연하게 대응되게
     var subscriptions = Set<AnyCancellable>()
     var didSelect = PassthroughSubject<AppleFramework, Never>()
+    //    방법2 - @Published보다, subject가 용이할때도 있음
+    var items = CurrentValueSubject<[AppleFramework], Never>(AppleFramework.list)// 데이터를 첨만 갖고있다가, 나중에 변경될때
+    //    방법1
+    //    @Published var list: [AppleFramework] = AppleFramework.list
     
     // Data, Presentation, Layout
     override func viewDidLoad() {
@@ -32,16 +35,13 @@ class FrameworkListViewController: UIViewController {
         // collectionView 설정 - presentation, layout
         // collectionView data 설정
         configureCollectionView()
-        
-        
-        // 위임하는데, 이 FrameworkListViewController에 위임하겠다.
-        collectionView.delegate = self
+        bind()
     }
     
     private func bind() { // point logic
         // ** 이 어플에서 대응해야할 input, output 소스
         
-        // input : 사용자 input을 받아서 처리해야할 것
+        // 1. input : 사용자 input을 받아서 처리해야할 것
         // - item선택 되었을때 처리
         didSelect
             .receive(on: RunLoop.main) // Scheduler : UI update이니, 메인 스레드에서일어나게.
@@ -53,8 +53,14 @@ class FrameworkListViewController: UIViewController {
             self.present(vc, animated: true)
         }.store(in: &subscriptions) // subscription이 생겼으니, subscriptions통에 넣어준다.
         
-        // output: data / status 변경에따라, UI update할것
+        // 2. output: data / status 변경에따라, UI update할것
         // items가 새로 세팅되었을때 (서버에서 받아서 등 어떤형태로든 업데이트 됐을경우)
+        // 방법1 - $list
+        items // 방법2
+            .receive(on: RunLoop.main) // main thread에서 작동해야하니, Scheduler 설정해주기
+            .sink { [unowned self] list in
+            self.applySectionItems(list)
+        }.store(in: &subscriptions) // 또 만들어진 subscription -> subscriptions통으로 넣어놓기
     }
     
     private func applySectionItems(_ items: [Item], to section: Section = .main) {
@@ -78,6 +84,7 @@ class FrameworkListViewController: UIViewController {
         // layout
         collectionView.collectionViewLayout = layout()
         
+        collectionView.delegate = self
     }
     
     private func layout() -> UICollectionViewCompositionalLayout {
@@ -103,7 +110,10 @@ class FrameworkListViewController: UIViewController {
 extension FrameworkListViewController: UICollectionViewDelegate {
     // item이 selected됐을때 호출되는 메소드
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let framework = list[indexPath.item]
+        // 방법1
+        // let framework = list[indexPath.item]
+        // 방법2
+        let framework = items.value[indexPath.item]
         print(">>> selected: \(framework.name)")
         
         // didSelect Publisher에 data 보내기
