f48c03d7e1ea6b2f2acc0d4b71c775ee7a03a434
Hakyung Sohn <hortenssiaa@gmail.com>
2022-10-31 17:49:02 +0900

    commit show image, n/a

--- Github-User-Profile/GithubUserProfile/SearchViewController.swift
+++ Github-User-Profile/GithubUserProfile/SearchViewController.swift
@@ -18,7 +18,7 @@ class UserProfileViewController: UIViewController {
     // network
     
     /*
-     private(set) : 외부에서는 읽기만 가능(getter), 내부에서는 읽기/쓰기 모두 가능 (setter/getter)
+     private(set) : 외부에서는 읽기만 가능(getter), 내부에서는 읽기/쓰기 모두 가능 (setter/getter) 간소화된 버전
      - user 데이터가 update 될때마다 setting
      - network에서 갖고온 user가 nil 일수도 있어서 optional
      */
@@ -48,12 +48,13 @@ class UserProfileViewController: UIViewController {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.searchBar.placeholder = "hortenssiaa"
-        searchController.searchResultsUpdater = self // searchbar의 text detection
-        searchController.searchBar.delegate = self // searchbar에서 search button 클릭시의 value detection
+        searchController.searchResultsUpdater = self // ** searchbar의 text detection
+        searchController.searchBar.delegate = self // ** searchbar에서 search button 클릭시의 value detection
         self.navigationItem.searchController = searchController
     }
     
     // user update 될때마다 UI가 update 되게 >> user가 @Published 가 돼야한다.
+    // data update
     private func bind() {
         $user
             .sink { [unowned self] result in
@@ -61,6 +62,7 @@ class UserProfileViewController: UIViewController {
             }.store(in: &subscription)
     }
     
+    // data update detail
     private func updateUser(_ user: UserProfile?) {
         guard let user = user else {
             self.nameLabel.text = "n/a"
@@ -72,12 +74,20 @@ class UserProfileViewController: UIViewController {
         }
         self.nameLabel.text = user.name
         self.loginLabel.text = user.login
-        self.thumbnail.image = nil
         self.followerLabel.text = "follower: \(user.followers)"
         self.followingLabel.text = "following: \(user.following)"
+        // image url (avatar_utl) -> img
+        /*
+         * Kingfisher : Kingfisher is a library for downloading and caching images from the web.
+         * 使い方
+            let url = URL(string: "https://example.com/image.png")
+            imageView.kf.setImage(with: url)
+         */
+        self.thumbnail.kf.setImage(with: user.avatarUrl)
     }
 }
 
+// searchResultsUpdater extension
 extension UserProfileViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         let keyword = searchController.searchBar.text
@@ -85,6 +95,7 @@ extension UserProfileViewController: UISearchResultsUpdating {
     }
 }
 
+// searchBar.delegate extension
 extension UserProfileViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         // network 요청 & 받기
@@ -96,12 +107,15 @@ extension UserProfileViewController: UISearchBarDelegate {
         let params: [String: String] = [:] // 3. parameter (optional)
         let header: [String: String] = ["Content-Type": "application/json"] // 4.
         
+        // making url 1. 기본 url components : base + path
         var urlComponents = URLComponents(string: base + path)!
+        // making url 2. add parameter
         let queryItem = params.map { (key: String, value: String) in
             URLQueryItem(name: key, value: value)
         }
         urlComponents.queryItems = queryItem
         
+        // making url 3. add header
         // url request에 필요한것 : url base + url path + url params + url header
         var request = URLRequest(url: urlComponents.url!) // http request시 header추가
         header.forEach { (key: String, value: String) in // header에 필요한 내용들 돌어가면서 넣기
@@ -122,6 +136,11 @@ extension UserProfileViewController: UISearchBarDelegate {
             .decode(type: UserProfile.self, decoder: JSONDecoder())
             .receive(on: RunLoop.main)
             .sink { completion in
+                switch completion {
+                case .failure(let error): // error인경우, user에 nil
+                    self.user = nil
+                case .finished: break
+                }
                 print("completion: \(completion)")
             } receiveValue: { user in
                 self.user = user
