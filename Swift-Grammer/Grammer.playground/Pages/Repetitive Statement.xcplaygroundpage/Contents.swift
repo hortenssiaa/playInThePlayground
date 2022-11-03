145f6c386f99c967fbfb90b7663d310182e2b55f
Hakyung Sohn <hortenssiaa@gmail.com>
2022-11-03 14:17:48 +0900

    commit repetitive statement

--- Swift-Grammer/Grammer.playground/Pages/Repetitive Statement.xcplaygroundpage/Contents.swift
+++ Swift-Grammer/Grammer.playground/Pages/Repetitive Statement.xcplaygroundpage/Contents.swift
@@ -1,7 +1,45 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+/* 1
+ for 루프상수 in 순회대상 {
+    // 실행할 구문..
+ }
+ */
 
-var greeting = "Hello, playground"
+for i in 1...4 {
+    print("i : \(i)")
+}
 
-//: [Next](@next)
+let array = [10, 20, 30, 40, 50,]
+for j in array { // Array, Dictionary, Set, String도 순회대상으로 사용 가능!
+    print("j : \(j)")
+}
+
+
+/* 2
+ while 조건식 {
+    // 실행할 구문
+ }
+ */
+
+var number = 5
+while number < 10 {
+    number += 1
+}
+number
+
+
+/* 3
+ repeat-while (== do-while)
+ repeat {
+    // 실행할 구문
+ } while 조건식
+ */
+
+var x = 6
+
+repeat {
+    x += 2
+} while x < 5
+
+print("x: \(x)")
