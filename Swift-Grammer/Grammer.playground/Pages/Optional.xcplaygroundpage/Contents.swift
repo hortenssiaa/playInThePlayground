c3256d8aac1931497f03abfc93b4344ce53ad1a2
Hakyung Sohn <hortenssiaa@gmail.com>
2022-11-03 14:27:25 +0900

    commit optional

---
+++ Swift-Grammer/Grammer.playground/Pages/Optional.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var name: String?
+
+var optionalName: String? = "Hakyung"
+print(optionalName) // Optional("Hakyung")
+
+//var requiredName: String = optionalName
+// error : Value of optional type 'String?' must be unwrapped to a value of type 'String'
